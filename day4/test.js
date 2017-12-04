38c533b16ddb5bd6d346f0bc9ae2f60858609813
Brad Davies <brad93@gmx.com>
2017-12-04 10:25:35 +0000

    Day 4 - Add Tests

--- day4/test.js
+++ day4/test.js
@@ -1,6 +1,6 @@
 const expect = require('unexpected')
 
-const { solve1 } = require('./solve')
+const { solve1, solve2 } = require('./solve')
 
 describe('Day 4', function() {
   describe('Part 1', function() {
@@ -16,6 +16,24 @@ describe('Day 4', function() {
         'to be',
         1
       )
+    })
+  })
+  describe('Part 2', function() {
+    it('Example 1', function() {
+      expect(solve2('aa bb cc dd ee\naa ba ab ck dm'), 'to be', 1)
+    })
+    it('Example 2', function() {
+      expect(solve2('aa cc ss ww ka\nke rk kw iii ooo'), 'to be', 2)
+    })
+    it('Example 3', function() {
+      expect(
+        solve2('abcde fghij\nabcde xyz ecdab\na ab abc abd abf abj'),
+        'to be',
+        2
+      )
+    })
+    it('Example 4', function() {
+      expect(solve2('iiii oiii ooii oooi\noiii ioii iioi iiio'), 'to be', 1)
     })
   })
 })
