6f01a9367751b248fa31c24accf55204e5e07cc5
Brad Davies <brad93@gmx.com>
2017-12-04 10:22:49 +0000

    Day 4 - P2

--- day4/index.js
+++ day4/index.js
@@ -1,10 +1,11 @@
 const fs = require('fs')
 const path = require('path')
 
-const { solve1 } = require('./solve')
+const { solve1, solve2 } = require('./solve')
 
 const input = fs.readFileSync(path.join(__dirname, 'input.txt'), {
   encoding: 'utf-8'
 })
 
 console.log('Day 4, Part 1:', solve1(input))
+console.log('Day 4, Part 2:', solve2(input))
--- day4/solve.js
+++ day4/solve.js
@@ -13,6 +13,27 @@ function solve1(n) {
     .reduce((prev, cur) => (prev += cur ? 1 : 0), 0)
 }
 
+function solve2(n) {
+  let lines = n.split('\n')
+  return lines
+    .map(line => {
+      line = line.split(' ').map(word =>
+        word
+          .split('')
+          .sort()
+          .join('')
+      )
+      for (let i = 0; i < line.length - 1; ++i) {
+        for (let j = i + 1; j < line.length; ++j) {
+          if (line[i] === line[j]) return false
+        }
+      }
+      return true
+    })
+    .reduce((prev, cur) => (prev += cur ? 1 : 0), 0)
+}
+
 module.exports = {
-  solve1
+  solve1,
+  solve2
 }
