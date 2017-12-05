7357cdf04f6a926b0c85a9a6b1b28542834a765c
Brad Davies <brad93@gmx.com>
2017-12-05 21:06:03 +0000

    Day 5 - Slightly compressing stuff for some reason

--- day5/solve.js
+++ day5/solve.js
@@ -1,25 +1,21 @@
 function solve1(n) {
-  let data = n.split('\n').map(Number)
+  const data = n.split('\n').map(Number)
   let i = 0
   let ticks = 0
-  while (1) {
-    data[i]++
-    i += data[i] - 1
-    ticks++
+  while (++ticks) {
+    i += data[i]++
     if (i < 0 || i >= data.length) break
   }
   return ticks
 }
 
 function solve2(n) {
-  let data = n.split('\n').map(Number)
+  const data = n.split('\n').map(Number)
   let i = 0
   let ticks = 0
-  while (1) {
+  while (++ticks) {
     const v = data[i] >= 3 ? -1 : 1
-    data[i] += v
-    i += data[i] - v
-    ticks++
+    i += (data[i] += v) - v
     if (i < 0 || i >= data.length) break
   }
   return ticks
