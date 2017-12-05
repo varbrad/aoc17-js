08a40faa9e67c6f107a78ad667ce24439eaafdff
Brad Davies <brad93@gmx.com>
2017-12-05 13:28:00 +0000

    Time solve functions

--- day5/index.js
+++ day5/index.js
@@ -7,5 +7,13 @@ const input = fs.readFileSync(path.join(__dirname, 'input.txt'), {
   encoding: 'utf-8'
 })
 
-console.log('Day 5, Part 1:', solve1(input))
-console.log('Day 5, Part 2:', solve2(input))
+console.time('solve1()')
+const p1 = solve1(input)
+console.timeEnd('solve1()')
+
+console.time('solve2()')
+const p2 = solve2(input)
+console.timeEnd('solve2()')
+
+console.log('Day 5, Part 1:', p1)
+console.log('Day 5, Part 2:', p2)
