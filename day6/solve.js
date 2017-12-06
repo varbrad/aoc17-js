2d8e397e9767ce5839c1098c8d7706ae7186e22c
Brad Davies <brad93@gmx.com>
2017-12-06 10:04:51 +0000

    Day 6 - Much faster Part 2 solver

--- day6/solve.js
+++ day6/solve.js
@@ -1,4 +1,4 @@
-function solve1(n) {
+function cycleSolver(n) {
   let state = n.split('\t').map(Number)
   let cycles = 0
   let map = {}
@@ -13,25 +13,15 @@ function solve1(n) {
     }
     cycles++
   }
-  return cycles
+  return [cycles, state]
+}
+
+function solve1(n) {
+  return cycleSolver(n)[0]
 }
 
 function solve2(n) {
-  let state = n.split('\t').map(Number)
-  let map = {}
-  while (map[state.join(',')] === undefined) {
-    map[state.join(',')] = 0
-    // Find biggest index
-    let [v, i] = state.reduce((p, c, i) => (c > p[0] ? [c, i] : p), [-1, -1])
-    state[i] = 0
-    // From index+1, keep increasing
-    while (v--) {
-      state[++i >= state.length ? (i = 0) : i]++
-    }
-    //
-    for (k in map) map[k]++
-  }
-  return map[state.join(',')]
+  return cycleSolver(cycleSolver(n)[1].join('\t'))[0]
 }
 
 module.exports = {
