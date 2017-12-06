b3dd2c47b52fc14c00a87e62653a244c9f209d1e
Brad Davies <brad93@gmx.com>
2017-12-06 10:10:34 +0000

    Day 6 - Slightly code-golf cycleSolver

--- day6/solve.js
+++ day6/solve.js
@@ -1,19 +1,16 @@
-function cycleSolver(n) {
-  let state = n.split('\t').map(Number)
-  let cycles = 0
-  let map = {}
-  while (map[state.join(',')] !== 1) {
-    map[state.join(',')] = 1
-    // Find biggest index
-    let [v, i] = state.reduce((p, c, i) => (c > p[0] ? [c, i] : p), [-1, -1])
-    state[i] = 0
-    // From index+1, keep increasing
-    while (v--) {
-      state[++i >= state.length ? (i = 0) : i]++
-    }
-    cycles++
+function cycleSolver(e) {
+  let data = e.split('\t').map(Number),
+    cyc = 0,
+    map = {}
+  for (; 1 !== map[data.join(',')]; ) {
+    map[data.join(',')] = 1
+    // Find biggest index and value
+    let [i, v] = data.reduce((e, r, t) => (r > e[0] ? [r, t] : e), [-1, -1])
+    // Distrib values
+    for (data[v] = 0; i--; ) data[++v >= data.length ? (v = 0) : v]++
+    cyc++
   }
-  return [cycles, state]
+  return [cyc, data]
 }
 
 function solve1(n) {
