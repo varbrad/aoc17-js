7aa03b6644cd0b7427e00756107367129e3c27fc
Brad Davies <varbrad@users.noreply.github.com>
2017-12-08 11:20:36 +0000

    Day 7 - P2 & Tests

--- day7/solve.js
+++ day7/solve.js
@@ -11,6 +11,63 @@ function solve1(n) {
   return null
 }
 
+function solve2(n) {
+  const re = /(\w+) \((\d+)\)( -> ([\w, ]+))?/
+  n = n.split('\n').map(v => re.exec(v))
+  let map = {}
+  n.forEach(
+    v =>
+      (map[v[1]] = {
+        id: v[1],
+        weight: Number(v[2]),
+        children: v[4] ? v[4].split(', ') : null,
+        sum: function() {
+          return (
+            this.weight +
+            (this.children !== null
+              ? this.children.reduce((p, c) => p + c.sum(), 0)
+              : 0)
+          )
+        }
+      })
+  )
+  for (let id in map) {
+    const o = map[id]
+    if (o.children) {
+      o.children = o.children.map(id => map[id])
+    }
+  }
+  let root = null
+  // Get the root node
+  for (let id in map)
+    root === null || map[id].sum() > root.sum() ? (root = map[id]) : null
+  // Now work backwards, which 'child' has the incorrect weight
+  let [wrongNode, targetWeight] = getWrongChild(root)
+  let diff = wrongNode.sum() - targetWeight
+  wrongNode.weight -= diff
+  return wrongNode.weight
+}
+
+function getWrongChild(node, targetWeight) {
+  if (node.children === null) return [node, targetWeight]
+  let map = {}
+  node.children.forEach(child => {
+    let sum = child.sum()
+    if (map[sum]) map[sum].count++
+    else map[sum] = { node: child, count: 1 }
+  })
+  let min = null,
+    weight = -1
+  for (let sum in map) {
+    if (map[sum].count === 1) min = map[sum].node
+    else weight = Number(sum)
+  }
+  // No unbalance in lower children, so inbalance must be this node!
+  if (min === null) return [node, targetWeight]
+  return getWrongChild(min, weight)
+}
+
 module.exports = {
-  solve1
+  solve1,
+  solve2
 }
--- day7/test.js
+++ day7/test.js
@@ -1,9 +1,30 @@
 const expect = require('unexpected')
 
-const { solve1 } = require('./solve')
+const { solve1, solve2 } = require('./solve')
+
+let testData = `pbga (66)
+xhth (57)
+ebii (61)
+havc (66)
+ktlj (57)
+fwft (72) -> ktlj, cntj, xhth
+qoyq (66)
+padx (45) -> pbga, havc, qoyq
+tknk (41) -> ugml, padx, fwft
+jptl (61)
+ugml (68) -> gyxo, ebii, jptl
+gyxo (61)
+cntj (57)`
 
 describe('Day 7', function() {
   describe('Part 1', function() {
-    it('Example 1', function() {})
+    it('Example 1', function() {
+      expect(solve1(testData), 'to be', 'tknk')
+    })
+  })
+  describe('Part 2', function() {
+    it('Example 1', function() {
+      expect(solve2(testData), 'to be', 60)
+    })
   })
 })
