8b418ed480c75b09169fc789830281d357e34405
Brad Davies <varbrad@users.noreply.github.com>
2017-12-09 11:56:38 +0000

    Day 9 - Tests for P2

--- day9/test.js
+++ day9/test.js
@@ -1,6 +1,6 @@
 const expect = require('unexpected')
 
-const { solve1 } = require('./solve')
+const { solve1, solve2 } = require('./solve')
 
 describe('Day 9', function() {
   describe('Part 1', function() {
@@ -27,6 +27,29 @@ describe('Day 9', function() {
     })
     it('Example 8', function() {
       expect(solve1('{{<a!>},{<a!>},{<a!>},{<ab>}}'), 'to be', 3)
+    })
+  })
+  describe('Part 2', function() {
+    it('Example 1', function() {
+      expect(solve2('<>'), 'to be', 0)
+    })
+    it('Example 2', function() {
+      expect(solve2('<random characters>'), 'to be', 17)
+    })
+    it('Example 3', function() {
+      expect(solve2('<<<<>'), 'to be', 3)
+    })
+    it('Example 4', function() {
+      expect(solve2('<{!>}>'), 'to be', 2)
+    })
+    it('Example 5', function() {
+      expect(solve2('<!!>'), 'to be', 0)
+    })
+    it('Example 6', function() {
+      expect(solve2('<!!!>>'), 'to be', 0)
+    })
+    it('Example 7', function() {
+      expect(solve2('<{o"i!a,<{i<a>'), 'to be', 10)
     })
   })
 })
