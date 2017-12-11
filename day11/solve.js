345ca46a5202f1b9e72502ccd2463fde03e2db51
Brad Davies <varbrad@users.noreply.github.com>
2017-12-11 07:53:06 +0000

    Day 11 - P2

--- day11/solve.js
+++ day11/solve.js
@@ -27,6 +27,40 @@ function solve1(input) {
   return Math.abs(ne * 0.5 - nw * 0.5) + Math.abs(ne * 0.5 + nw * 0.5 + n)
 }
 
+function solve2(input) {
+  let nw = 0,
+    n = 0,
+    ne = 0,
+    maxDist = 0
+  input.split(',').forEach(dir => {
+    switch (dir) {
+      case 'n':
+        n++
+        break
+      case 'ne':
+        ne++
+        break
+      case 'se':
+        nw--
+        break
+      case 's':
+        n--
+        break
+      case 'sw':
+        ne--
+        break
+      case 'nw':
+        nw++
+        break
+    }
+    const dist =
+      Math.abs(ne * 0.5 - nw * 0.5) + Math.abs(ne * 0.5 + nw * 0.5 + n)
+    if (dist > maxDist) maxDist = dist
+  })
+  return maxDist
+}
+
 module.exports = {
-  solve1
+  solve1,
+  solve2
 }
