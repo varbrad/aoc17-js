58d1eb5aa932e9ef47a1f21c4114abb6c1afeaff
Brad Davies <varbrad@users.noreply.github.com>
2017-12-15 12:03:21 +0000

    Day 14 - P2

--- day14/index.js
+++ day14/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 
-const { solve1 } = require('./solve')
+const { solve1, solve2 } = require('./solve')
 
 const input = 'wenycdww'
 
@@ -9,4 +9,9 @@ console.time('solve1()')
 const p1 = solve1(input)
 console.timeEnd('solve1()')
 
+console.time('solve2()')
+const p2 = solve2(input)
+console.timeEnd('solve2()')
+
 console.log('Day 14, Part 1:', p1)
+console.log('Day 14, Part 2:', p2)
--- day14/solve.js
+++ day14/solve.js
@@ -16,6 +16,44 @@ function solve1(n) {
   return filled
 }
 
+function solve2(n) {
+  let map = []
+  for (let i = 0; i < 128; ++i) {
+    const hash = day10.solve2(256, n + '-' + i)
+    map[i] = []
+    let data = ''
+    for (let k = 0; k < hash.length; k += 2) {
+      // For every byte
+      let bin = parseInt(hash.slice(k, k + 2), 16).toString(2)
+      while (bin.length < 8) bin = '0' + bin
+      // Get count of 1's
+      data += bin
+    }
+    map[i] = data.split('').map(v => (v === '1' ? 1 : 0))
+  }
+  //
+  let groups = 0
+  for (let y = 0; y < map.length; ++y) {
+    for (let x = 0; x < map[y].length; ++x) {
+      if (map[y][x] === 0) continue
+      let toCheck = [[x, y]]
+      while (toCheck.length > 0) {
+        let [cX, cY] = toCheck.pop()
+        if (map[cY][cX] === 0) continue
+        //
+        map[cY][cX] = 0
+        if (map[cY] && map[cY][cX - 1]) toCheck.push([cX - 1, cY])
+        if (map[cY] && map[cY][cX + 1]) toCheck.push([cX + 1, cY])
+        if (map[cY - 1] && map[cY - 1][cX]) toCheck.push([cX, cY - 1])
+        if (map[cY + 1] && map[cY + 1][cX]) toCheck.push([cX, cY + 1])
+      }
+      groups++
+    }
+  }
+  return groups
+}
+
 module.exports = {
-  solve1
+  solve1,
+  solve2
 }
