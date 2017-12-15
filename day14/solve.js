20b8d25e473439b6ac46ede6f35194c87738311f
Brad Davies <varbrad@users.noreply.github.com>
2017-12-15 12:04:45 +0000

    Day 14 - P2 Comments

--- day14/solve.js
+++ day14/solve.js
@@ -35,18 +35,25 @@ function solve2(n) {
   let groups = 0
   for (let y = 0; y < map.length; ++y) {
     for (let x = 0; x < map[y].length; ++x) {
+      // If this cell is 0, just continue
       if (map[y][x] === 0) continue
       let toCheck = [[x, y]]
       while (toCheck.length > 0) {
         let [cX, cY] = toCheck.pop()
+        // Ignore if this cell already changed
         if (map[cY][cX] === 0) continue
-        //
+        // Set this cell to 0
         map[cY][cX] = 0
+        // Check to left if cell is in this group
         if (map[cY] && map[cY][cX - 1]) toCheck.push([cX - 1, cY])
+        // Check to right
         if (map[cY] && map[cY][cX + 1]) toCheck.push([cX + 1, cY])
+        // Up
         if (map[cY - 1] && map[cY - 1][cX]) toCheck.push([cX, cY - 1])
+        // Down
         if (map[cY + 1] && map[cY + 1][cX]) toCheck.push([cX, cY + 1])
       }
+      // Group exhausted, increment group count
       groups++
     }
   }
