75b458a4ca02dd78710ef0c17ce43e8d16e08877
Brad Davies <varbrad@users.noreply.github.com>
2017-12-15 12:50:27 +0000

    Day 15 - P2

--- day15/index.js
+++ day15/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 
-const { solve1 } = require('./solve')
+const { solve1, solve2 } = require('./solve')
 
 const [a, b] = [116, 299]
 
@@ -9,4 +9,9 @@ console.time('solve1()')
 const p1 = solve1(a, b)
 console.timeEnd('solve1()')
 
+console.time('solve2()')
+const p2 = solve2(a, b)
+console.timeEnd('solve2()')
+
 console.log('Day 15, Part 1:', p1)
+console.log('Day 15, Part 2:', p2)
--- day15/solve.js
+++ day15/solve.js
@@ -1,7 +1,8 @@
-function* gen(start, factor) {
+function* gen(start, factor, mod) {
   let a = start
   while (1) {
     a = (a * factor) % 2147483647
+    if (mod && a % mod !== 0) continue
     yield a & ((1 << 16) - 1)
   }
 }
@@ -15,6 +16,16 @@ function solve1(a, b) {
   return c
 }
 
+function solve2(a, b) {
+  const genA = gen(a, 16807, 4)
+  const genB = gen(b, 48271, 8)
+  let c = 0
+  for (let i = 0; i < 5000000; ++i)
+    if (genA.next().value === genB.next().value) c++
+  return c
+}
+
 module.exports = {
-  solve1
+  solve1,
+  solve2
 }
