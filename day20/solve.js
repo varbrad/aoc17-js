7de504fbac7f71911fba79bf90df90373e501438
Brad Davies <varbrad@users.noreply.github.com>
2017-12-20 11:39:25 +0000

    Day 20 - P2

--- day20/solve.js
+++ day20/solve.js
@@ -38,6 +38,50 @@ function solve1(n) {
   return cP.id
 }
 
+function solve2(n) {
+  const regex = /p=<(-?\d+),(-?\d+),(-?\d+)>, v=<(-?\d+),(-?\d+),(-?\d+)>, a=<(-?\d+),(-?\d+),(-?\d+)>/
+  n = n.split('\n').map((l, i) => {
+    const data = regex
+      .exec(l)
+      .slice(1, 10)
+      .map(Number)
+
+    return {
+      id: i,
+      p: data.slice(0, 3),
+      v: data.slice(3, 6),
+      a: data.slice(6, 9)
+    }
+  })
+
+  let loops = 0
+  while (++loops < 50) {
+    n.forEach(p => {
+      for (let i = 0; i < 3; ++i) {
+        p.v[i] += p.a[i]
+        p.p[i] += p.v[i]
+      }
+    })
+    let toRemove = {}
+    for (let i = 0; i < n.length - 1; ++i) {
+      const p1 = n[i]
+      for (let j = i + 1; j < n.length; ++j) {
+        const p2 = n[j]
+        if (p1.p[0] === p2.p[0] && p1.p[1] === p2.p[1] && p1.p[2] === p2.p[2]) {
+          toRemove[p1.id] = true
+          toRemove[p2.id] = true
+        }
+      }
+    }
+    for (let id in toRemove) {
+      n.splice(n.findIndex(p => p.id === id), 1)
+    }
+  }
+
+  return n.length
+}
+
 module.exports = {
-  solve1
+  solve1,
+  solve2
 }
