244407541a853f5d1b46275e18595034d2c7d4ca
Brad Davies <varbrad@users.noreply.github.com>
2017-12-21 23:36:44 +0000

    Day 21 - P2

--- day21/index.js
+++ day21/index.js
@@ -15,4 +15,9 @@ console.time('solve1()')
 const p1 = solve1(start, input, 5)
 console.timeEnd('solve1()')
 
+console.time('solve2()')
+const p2 = solve1(start, input, 18)
+console.timeEnd('solve2()')
+
 console.log('Day 21, Part 1:', p1)
+console.log('Day 21, Part 2:', p2)
